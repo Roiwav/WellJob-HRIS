6778d3225887bf08c9d84260e1cac77f032b97e7
Shi <shirleendalman123@gmail.com>
2026-04-09 18:52:56 +0800

    update code for kpi and SAP

--- frontend/src/pages/SuperAdminPortal.jsx
+++ frontend/src/pages/SuperAdminPortal.jsx
@@ -109,7 +109,7 @@ export default function SuperAdminPortal() {
 
               <div>
                 <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-                  Username
+                  Temporary Username
                 </label>
                 <input
                   type="text"
